6be5ad0b8937d6fb3e3810311044ad1de315a901
ChemaPool <jose_development@hotmail.com>
2019-12-22 13:56:22 -0600

    database_cleaner

--- spec/factories/home.rb
+++ spec/factories/home.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
   factory :home do
     price { Faker::Number.decimal }
     extra_service { Faker::Number.decimal }
-    total_amount { Faker::Number.decimal }
-    home_features Hash[garden: FFaker::Boolean.random, furnished: FFaker::Boolean.random, gym: FFaker::Boolean.random]
+    home_features { Hash[garden: FFaker::Boolean.random, furnished: FFaker::Boolean.random, gym: FFaker::Boolean.random] }
   end
 end
