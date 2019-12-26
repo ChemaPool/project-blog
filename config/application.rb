58c0790b60db2cdf7864607ec6b27c9633a83485
ChemaPool <jose_development@hotmail.com>
2019-12-25 22:53:05 -0600

    rails_job

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module Blog
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.active_job.queue_adapter = :sidekiq
   end
 end
